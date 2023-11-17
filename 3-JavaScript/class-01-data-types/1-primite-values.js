719fde1b38b32fbddc0e0601dfe5c60dde51abf8
Amilton Lucas <amilton.miguel786@gmail.com>
2023-11-17 15:27:32 -0300

    add files

---
+++ 3-JavaScript/class-01-data-types/1-primite-values.js
@@ -0,0 +1,44 @@
+console.log("Hello World!!!");
+
+// let 123num;
+// let num-1
+// let num 1;
+// let num#1;
+let num$1 = 0;
+// let númeroç;
+// let if;
+// let break;
+
+//string
+
+("String");
+("Lucas");
+
+let studentName = "Lucas";
+let age = 18;
+
+console.log(student);
+console.log(typeof student);
+console.log(typeof age);
+
+// Number: Integer , Float, NaN, Infinity
+console.log(typeof 30);
+console.log(typeof 30.5);
+console.log(2 + 3);
+console.log(2 + "3");
+console.log(typeof NaN);
+console.log(typeof Infinity);
+
+let number;
+console.log(number);
+
+// Boolean: true or false
+console.log(typeof true);
+console.log(20 == 20);
+console.log(20 == 30);
+console.log(20 == "20");
+console.log(20 === "20");
+console.log(20 != 10);
+
+let userName = "Amilton"
+console.log(userName);
---
+++ 3-JavaScript/class-01-data-types/2-template-literals.js
@@ -0,0 +1,23 @@
+console.log('test');
+console.log("test");
+console.log(`test`);
+
+let course = "Desenvolvimento Web"
+console.log("Turma de " + course);
+// ⬆ concatenation > concatenação ⬆
+
+let num1 = "10";
+let num2 = 20;
+
+num1 = Number(num1);
+console.log(num1 + num2);
+// ⬆ type coersion > digita coerção ⬆
+
+console.log("A soma de Num1 + Num2 é: " + (num1 = num2)); 
+// ⬆ grouping operator > Operador de Grupo ⬆
+
+console.log("A soma de " + num1 + " e " + num2 + " é: " + (num1 + num2));
+// ⬆ interpolation > Interpolação ⬆
+
+console.log(`A soma de ${num1} e ${num2} é: ${num1 + num2}`);
+// ⬆ template literals > Literais de Modelo ⬆
