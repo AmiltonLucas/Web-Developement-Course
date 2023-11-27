c655ab1b144b8f07aae2702ce7e402e88230c29c
Amilton Lucas <amilton.miguel786@gmail.com>
2023-11-27 15:59:18 -0300

    add file

--- 3-JavaScript/class-02-operators/2-comparison.js
+++ 3-JavaScript/class-02-operators/2-comparison.js
@@ -1,10 +1,10 @@
 let num1 = 2;
 let num2 = 3;
 
-console.log(num1 > num2);
-console.log(num1 < num2);
-console.log(num1 >= 2);
-console.log(num1 <= 3);
+console.log(num1 > num2); // maior que
+console.log(num1 < num2); // menor que
+console.log(num1 >= 2); // maior que ou igual
+console.log(num1 <= 3); // menor que ou igual
 console.log(num1 == num2);
 console.log(num1 != num2);
 console.log(num1 == "2");
