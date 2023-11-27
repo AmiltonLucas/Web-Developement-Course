d88a677237cd2282462d6febabfa79206c39abf3
Amilton Lucas <amilton.miguel786@gmail.com>
2023-11-27 15:58:10 -0300

    add files

---
+++ 3-JavaScript/class-02-operators/1-arithmetic.js
@@ -0,0 +1,39 @@
+let num1 = 81;
+let num2 = 3;
+
+console.log(num1 + num2); // Adição
+console.log(num1 - num2); // Subtração
+console.log(num1 * num2); // Mutiplicação
+console.log(num1 / num2); // Divisão  
+console.log(num1 % num2); // Resto da Divisão  
+console.log(num1 ** num2); // Potência   
+console.log(num1 ** (1 / 2)); // Raiz Quadrada   
+console.log(Math.sqrt(num1));
+
+console.log(++num2);
+console.log(num2++);
+console.log(num2++);
+console.log(num2);
+console.log(++num2);
+
+let num = 2;
+console.log(num);
+console.log(num--);
+console.log(num);
+console.log(--num);
+console.log(num);
+console.log(num++);
+
+// Assignment 
+
+let number;
+number = 3;
+ // number = number + 20;
+
+number += 2
+number -= 2
+number *= 2
+number /= 2
+
+
+console.log(number);
